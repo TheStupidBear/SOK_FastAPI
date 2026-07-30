3fb9e3addbe8358cd8f655958637ec5957659454
TheStupidBear <dimasik.6.27i@gmail.com>
2026-07-30 11:16:13 +0300

    добавил кнопку логин и страницу html login

--- web/user.py
+++ web/user.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, Request, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
 from typing import Annotated
 from datetime import timedelta
-from model.user import User, Token
+from model.user import Token
 import service.user as service
 from data.user import init_user, login_user
 
@@ -10,6 +12,16 @@ router = APIRouter(prefix="/user", tags=["user"])
 
 #создаем БД
 init_user()
+
+parent_dir = Path(__file__).resolve().parent.parent
+template_obj = Jinja2Templates(directory=f"{parent_dir}/template")
+
+@router.get("/login")
+async def login(request: Request):
+    return template_obj.TemplateResponse(
+        request=request,
+        name="login.html")
+
 
 @router.post("/token")
 async def login_for_access_token(
@@ -28,6 +40,8 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
 
 
 
